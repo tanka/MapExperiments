b089fbe01b845053fb81908044db465c74e1320a
tanka <sharma.tankanath@gmail.com>
2013-03-28 13:21:38 +0530

    renamed main activity to mapmainactivity

--- src/com/example/mapexperiment/GroundOverlayDemoActivity.java
+++ src/com/example/mapexperiment/GroundOverlayDemoActivity.java
@@ -1,9 +1,15 @@
 package com.example.mapexperiment;
 
+import java.io.IOException;
+import java.util.List;
+
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,69 +19,86 @@ import com.google.android.gms.maps.model.GroundOverlay;
 import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
 
-
 /**
  * This shows how to add a ground overlay to a map.
  */
-public class GroundOverlayDemoActivity extends FragmentActivity implements OnSeekBarChangeListener {
+public class GroundOverlayDemoActivity extends FragmentActivity implements
+		OnSeekBarChangeListener {
 
-    private static final int TRANSPARENCY_MAX = 100;
-    private static final LatLng NEWARK = new LatLng(40.714086, -74.228697);
+	private static final int TRANSPARENCY_MAX = 100;
+	private static final LatLng NEWARK = new LatLng(40.714086, -74.228697);
 
-    private GoogleMap mMap;
-    private GroundOverlay mGroundOverlay;
-    private SeekBar mTransparencyBar;
+	private GoogleMap mMap;
+	private GroundOverlay mGroundOverlay;
+	private SeekBar mTransparencyBar;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.ground_overlay_demo);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.ground_overlay_demo);
 
-        mTransparencyBar = (SeekBar) findViewById(R.id.transparencySeekBar);
-        mTransparencyBar.setMax(TRANSPARENCY_MAX);
-        mTransparencyBar.setProgress(0);
+		mTransparencyBar = (SeekBar) findViewById(R.id.transparencySeekBar);
+		mTransparencyBar.setMax(TRANSPARENCY_MAX);
+		mTransparencyBar.setProgress(0);
 
-        setUpMapIfNeeded();
-    }
+		setUpMapIfNeeded();
+	}
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+	@Override
+	protected void onResume() {
+		super.onResume();
+		setUpMapIfNeeded();
+	}
 
-    private void setUpMapIfNeeded() {
-        if (mMap == null) {
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            if (mMap != null) {
-                setUpMap();
-            }
-        }
-    }
+	private void setUpMapIfNeeded() {
+		if (mMap == null) {
+			mMap = ((SupportMapFragment) getSupportFragmentManager()
+					.findFragmentById(R.id.map)).getMap();
+			if (mMap != null) {
+				setUpMap();
+			}
+		}
+	}
 
-    private void setUpMap() {
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NEWARK, 11));
+	private void setUpMap() {
+		Geocoder geocoder = new Geocoder(getApplicationContext());
+		List<Address> addr = null;
+		try {
+			addr = geocoder.getFromLocationName("Bhutan", 5);
+			Toast.makeText(
+					getApplicationContext(),
+					addr.get(0).getCountryName() + " " , Toast.LENGTH_LONG).show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+//		LatLng newLatLng = new LatLng(addr.get(0).getLatitude(), addr.get(0)
+//				.getLongitude());
 
-        mGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
-                .image(BitmapDescriptorFactory.fromResource(R.drawable.newark_nj_1922)).anchor(0, 1)
-                .position(NEWARK, 8600f, 6500f));
+		mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(NEWARK, 11));
 
-        mTransparencyBar.setOnSeekBarChangeListener(this);
-    }
+		mGroundOverlay = mMap.addGroundOverlay(new GroundOverlayOptions()
+				.image(BitmapDescriptorFactory
+						.fromResource(R.drawable.newark_nj_1922)).anchor(0, 1)
+				.position(NEWARK, 8600f, 6500f));
 
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-    }
+		mTransparencyBar.setOnSeekBarChangeListener(this);
+	}
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-    }
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+	}
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if (mGroundOverlay != null) {
-            mGroundOverlay.setTransparency((float) progress / (float) TRANSPARENCY_MAX);
-        }
-    }
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+	}
+
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress,
+			boolean fromUser) {
+		if (mGroundOverlay != null) {
+			mGroundOverlay.setTransparency((float) progress
+					/ (float) TRANSPARENCY_MAX);
+		}
+	}
 }
--- src/com/example/mapexperiment/MapMainActivity.java
+++ src/com/example/mapexperiment/MapMainActivity.java
@@ -21,165 +21,90 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 
-
-public class MainActivity extends FragmentActivity implements LocationListener {
-
+public class MapMainActivity extends FragmentActivity implements LocationListener {
 	GoogleMap googleMap;
-
 	Button button;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-
 		super.onCreate(savedInstanceState);
-
 		setContentView(R.layout.activity_main);
-		
 		button = (Button) findViewById(R.id.button1);
-
 		// Getting Google Play availability status
-
 		int status = GooglePlayServicesUtil
 				.isGooglePlayServicesAvailable(getBaseContext());
-
 		// Showing status
-
 		if (status != ConnectionResult.SUCCESS) {
 			// Google Play Services are not available
 			int requestCode = 10;
-
 			Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this,
 					requestCode);
-
 			dialog.show();
-		} else
-
-		{ // Google Play Services are available
-
+		} else { // Google Play Services are available
 			// Getting reference to the SupportMapFragment of activity_main.xml
-
 			SupportMapFragment fm = (SupportMapFragment) getSupportFragmentManager()
 					.findFragmentById(R.id.map);
-
 			// Getting GoogleMap object from the fragment
-
 			googleMap = fm.getMap();
-
 			// Enabling MyLocation Layer of Google Map
-
 			googleMap.setMyLocationEnabled(true);
-
 			// Getting LocationManager object from System Service
 			// LOCATION_SERVICE
-
 			LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-
 			// Creating a criteria object to retrieve provider
-
-			Criteria criteria = new
-
-			Criteria();
-
+			Criteria criteria = new Criteria();
 			// Getting the name of the best provider
-
 			String provider = locationManager.getBestProvider(criteria, true);
-
 			// Getting Current Location
-
 			Location location = locationManager.getLastKnownLocation(provider);
-
 			if (location != null) {
-
 				onLocationChanged(location);
-
 			}
-
 			locationManager.requestLocationUpdates(provider, 20000, 0, this);
-
 		}
-
 	}
 
 	@Override
 	public void onLocationChanged(Location location) {
 		TextView tvLocation = (TextView) findViewById(R.id.tv_location);
-
 		// Getting latitude of the current location
-
 		double latitude = location.getLatitude();
-
 		// Getting longitude of the current location
-
-		double
-
-		longitude = location.getLongitude();
-
+		double longitude = location.getLongitude();
 		// Creating a LatLng object for the current location
-
 		LatLng latLng = new LatLng(latitude, longitude);
-
 		// Showing the current location in Google Map
-
 		googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
-
 		// Zoom in the Google Map
-
 		googleMap.animateCamera(CameraUpdateFactory.zoomTo(15));
-
 		// Setting latitude and longitude in the TextView tv_location
-
-		tvLocation
-				.setText("Latitude:" + latitude + ",  Longitude:" + longitude);
-
+		tvLocation.setText("Latitude:" + latitude + ",  Longitude:" + longitude);
 	}
-
 	@Override
 	public void onProviderDisabled(String provider) {
-
-		// TODO Auto-generated method stub
-
 	}
-
 	@Override
 	public void onProviderEnabled(String provider) {
-
-		// TODO Auto-generated method stub
-
 	}
-
 	@Override
 	public void onStatusChanged(String provider, int status, Bundle extras) {
-
-		// TODO Auto-generated method stub
-
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
-
 		getMenuInflater().inflate(R.menu.main, menu);
-
-		return
-
-		true;
-
+		return true;
 	}
 	
 	@Override
 	protected void onStart() {
 		// TODO Auto-generated method stub
 		super.onStart();
-		
 		button.setOnClickListener(new OnClickListener() {
-			
 			@Override
 			public void onClick(View arg0) {
 				Intent intent = new Intent(getApplicationContext(), GroundOverlayDemoActivity.class);
 				startActivity(intent);
-				
 			}
 		});
 	}
-
 }
